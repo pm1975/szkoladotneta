160f5f849817b3f2597244a6630bb7b4d2c0b697
PM <pm1975@windowslive.com>
2022-03-24 10:01:58 +0100

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    //PascalCase
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //camelCase
+            string firstName = "Piotr";
+
+            //UPPER_CASE
+            const double PI_NUMBER = 3.14;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("To jest nowy napis");
+            if (PI_NUMBER > 2)
+            {
+                WritePiNumber(PI_NUMBER);
+            }
+        }
+
+        private static string WritePiNumber(double pi)
+        {
+            return ("3.14");
+        }
+    }
+
+    public interface IProgram
+    {
+    }
+}
